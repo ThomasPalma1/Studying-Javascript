76ed240221b9b63c2297da2867a45d15a4788a10
Thomas Palma <thomaspalma777@gmail.com>
2021-04-20 22:38:37 -0300

    read file and dir

---
+++ src/nodejs-codes/fs/readFile.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+//não bloqueante
+fs.readFile('texto.txt', (err, data)=>{
+    if(err) throw err
+    // console.log(data.toString())
+
+})
+
+//bloqueante
+const texto = fs.readFileSync("texto.txt")
+
+console.log(`Texto: ${texto} `)
